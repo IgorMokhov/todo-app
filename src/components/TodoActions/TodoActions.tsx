7fac269b0c0803de9352743bac1d76847fc8d89a
Igor Mokhov <igormokhovid@gmail.com>
2025-01-22 13:37:55 +0300

    feat: added TodoActions and setFilter

---
+++ src/components/TodoActions/TodoActions.tsx
@@ -0,0 +1,32 @@
+import { Filters } from '../../types/filters';
+import { ITodo } from '../../types/todo';
+import { TodoFilters } from '../TodoFilters/TodoFilters';
+import styles from './TodoActions.module.css';
+
+interface ITodoActionsProps {
+  todos: ITodo[];
+  activeFilter: Filters;
+  setFilter: (filter: Filters) => void;
+}
+
+export const TodoActions = ({
+  todos,
+  activeFilter,
+  setFilter,
+}: ITodoActionsProps) => {
+  const activeTodosCount = todos.filter(
+    (todo) => todo.isCompleted === false
+  ).length;
+
+  if (!todos.length) return <></>;
+
+  return (
+    <div className={styles.actions}>
+      <span>{activeTodosCount} items left</span>
+      <TodoFilters activeFilter={activeFilter} setFilter={setFilter} />
+      <button className={styles.actions_clear_btn} onClick={() => {}}>
+        Clear completed
+      </button>
+    </div>
+  );
+};
--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -4,15 +4,16 @@ import { TodoForm } from '../TodoForm/TodoForm';
 import { TodoList } from '../TodoList/TodoList';
 import { v4 as uuidv4 } from 'uuid';
 import styles from './Todos.module.css';
+import { TodoActions } from '../TodoActions/TodoActions';
+import { Filters } from '../../types/filters';
 
 export const Todos = () => {
   const [todos, setTodos] = useState<ITodo[]>([]);
+  const [activeFilter, setActiveFilter] = useState<Filters>('all');
 
   const addTodo = (text: string) => {
     setTodos([...todos, { text, isCompleted: false, id: uuidv4() }]);
   };
-
-  console.log(todos);
 
   const toggleTodo = (id: string) => {
     setTodos(
@@ -22,11 +23,20 @@ export const Todos = () => {
     );
   };
 
+  const setFilter = (filter: Filters) => {
+    setActiveFilter(filter);
+  };
+
   return (
     <section className={styles.todos}>
       <h1 className={styles.todos_title}>todos</h1>
       <TodoForm addTodo={addTodo} />
-      <TodoList todos={todos} toggleTodo={toggleTodo}/>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <TodoActions
+        todos={todos}
+        activeFilter={activeFilter}
+        setFilter={setFilter}
+      />
     </section>
   );
 };
