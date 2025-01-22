ec208b1a53495f8ab3ebd8d22a484ebd48e5940e
Igor Mokhov <igormokhovid@gmail.com>
2025-01-22 14:54:31 +0300

    feat: added function getTodosByFilter

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ITodo } from '../../types/todo';
 import { TodoForm } from '../TodoForm/TodoForm';
 import { TodoList } from '../TodoList/TodoList';
 import { v4 as uuidv4 } from 'uuid';
-import styles from './Todos.module.css';
 import { TodoActions } from '../TodoActions/TodoActions';
 import { Filters } from '../../types/filters';
+import styles from './Todos.module.css';
 
 export const Todos = () => {
   const [todos, setTodos] = useState<ITodo[]>([]);
@@ -27,11 +27,27 @@ export const Todos = () => {
     setActiveFilter(filter);
   };
 
+  const getTodosByFilter = (filter: Filters, todos: ITodo[]) => {
+    switch (filter) {
+      case 'all':
+        return todos;
+      case 'active':
+        return todos.filter((todo) => !todo.isCompleted);
+      case 'completed':
+        return todos.filter((todo) => todo.isCompleted);
+      default:
+        break;
+    }
+  };
+
   return (
     <section className={styles.todos}>
       <h1 className={styles.todos_title}>todos</h1>
       <TodoForm addTodo={addTodo} />
-      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <TodoList
+        todos={getTodosByFilter(activeFilter, todos) || []}
+        toggleTodo={toggleTodo}
+      />
       <TodoActions
         todos={todos}
         activeFilter={activeFilter}
